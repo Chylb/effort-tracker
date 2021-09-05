a77728d24d53e0effc4b59191301733549bd4262
Tomasz Ociepka <tomipl200@o2.pl>
2021-09-05 10:54:42 +0200

    altitude chart has minimum span

--- frontend/src/pages/activity/AltitudeChart.jsx
+++ frontend/src/pages/activity/AltitudeChart.jsx
@@ -2,55 +2,67 @@
 import { Scatter } from 'react-chartjs-2';
 import { secondsToString } from '../../utils/secondsToString';
 
-const options = {
-    animation: {
-        duration: 0,
-    },
-    plugins: {
-        tooltip: {
-            mode: "nearest",
-            axis: "x",
-            intersect: true,
-            animationDuration: 0,
-            displayColors: false,
-            callbacks: {
-                label: (ctx) => {
-                    const datasetCount = ctx.chart.config._config.data.datasets.length;
-                    if (ctx.datasetIndex == 0 && datasetCount == 1 || ctx.datasetIndex == 1)
-                        return [
-                            "Time: " + secondsToString(ctx.raw.time),
-                            "Dist: " + ctx.raw.x,
-                            "Elev: " + ctx.raw.y]
+const generateOptions = (minmax) => {
+    const options = {
+        animation: {
+            duration: 0,
+        },
+        plugins: {
+            tooltip: {
+                mode: "nearest",
+                axis: "x",
+                intersect: true,
+                animationDuration: 0,
+                displayColors: false,
+                callbacks: {
+                    label: (ctx) => {
+                        const datasetCount = ctx.chart.config._config.data.datasets.length;
+                        if (ctx.datasetIndex == 0 && datasetCount == 1 || ctx.datasetIndex == 1)
+                            return [
+                                "Time: " + secondsToString(ctx.raw.time),
+                                "Dist: " + ctx.raw.x,
+                                "Elev: " + ctx.raw.y]
+                    },
+                }
+            },
+            legend: {
+                display: false
+            }
+        },
+        elements: {
+            point: {
+                radius: 0,
+                hitRadius: 1000,
+                hoverRadius: 0
+            }
+        },
+        scales: {
+            x: {
+                type: 'linear',
+                title: {
+                    display: true,
+                    text: 'Distance [m]'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'Elevation [m]'
                 },
-            }
-        },
-        legend: {
-            display: false
-        }
-    },
-    elements: {
-        point: {
-            radius: 0,
-            hitRadius: 1000,
-            hoverRadius: 0
-        }
-    },
-    scales: {
-        x: {
-            type: 'linear',
-            title: {
-                display: true,
-                text: 'Distance [m]'
-            }
-        },
-        y: {
-            title: {
-                display: true,
-                text: 'Elevation [m]'
+                ticks: {
+                    stepSize: 5
+                }
             }
         }
+    };
+
+    if (minmax != undefined) {
+        options.scales.y.min = minmax[0];
+        options.scales.y.max = minmax[1];
     }
-};
+
+    return options;
+}
 
 const getDataset = (data, isEffort) => {
     return {
@@ -61,6 +73,21 @@ const getDataset = (data, isEffort) => {
         type: 'line',
         fill: true,
     }
+}
+
+const optimalChartMinmax = (altitude) => {
+    const min = Math.min(...altitude);
+    const max = Math.max(...altitude);
+    const span = max - min;
+    const minSpan = 20;
+    if (span < minSpan) {
+        const deficit = minSpan - span;
+        const rounding = 5;
+        const newMin = Math.floor(min - deficit / 2);
+        const newMax = Math.ceil(max + deficit / 2) + 5;
+        return [newMin - newMin % rounding, newMax - newMax % rounding];
+    }
+    return;
 }
 
 export const AltitudeChart = props => {
@@ -84,7 +111,7 @@ export const AltitudeChart = props => {
 
     return (
         <>
-            <Scatter data={chartData} options={options} height={60} />
+            <Scatter data={chartData} options={generateOptions(optimalChartMinmax(props.streams.altitude.data))} height={60} />
         </>
     );
 }
